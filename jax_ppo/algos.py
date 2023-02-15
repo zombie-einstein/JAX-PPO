Fix entropy calculation and better handling of keys in runner

jax_ppo/algos.py
@@ -55,7 +55,11 @@ def calculate_losses(
     approx_kl = jnp.mean(ratio - 1.0 - log_ratio)
 
     # Entropy Loss
-    entropy = jnp.mean(-jnp.exp(new_log_likelihood) * new_log_likelihood, axis=0)
+    entropy = log_std + 0.5 * jnp.log(2 * jnp.pi * jnp.e)
+
+    if jnp.ndim(entropy) > 1:
+        entropy = jnp.sum(entropy, axis=1)
+    entropy = jnp.mean(entropy, axis=0)
 
     total_loss = (
         p_loss + ppo_params.critic_coeff * v_loss - ppo_params.entropy_coeff * entropy

jax_ppo/utils.py
@@ -26,7 +26,9 @@ def calculate_gae(
         return gae, gae
 
     _, advantages = jax.lax.scan(
-        _adv_scan, 0.0, (jnp.flip(delta, axis=0), jnp.flip(terminals, axis=0))
+        _adv_scan,
+        0.0,
+        (jnp.flip(delta, axis=0), jnp.flip(terminals, axis=0)),
     )
     advantages = jnp.flip(advantages)
     returns = advantages + trajectories.value[:-1]