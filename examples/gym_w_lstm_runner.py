8bad66091f2a451969c78dcb6be1803d8d9091fa
zombie-einstein <zombie-einstein@users.noreply.github.com>
2023-02-23 23:19:15 +0000

    Add velocity masked pendulum environment

--- examples/gym_w_lstm_runner.py
+++ examples/gym_w_lstm_runner.py
@@ -4,9 +4,23 @@ from functools import partial
 import jax
 import jax.numpy as jnp
 import jax_tqdm
-from gymnax.environments import environment
+from gymnax.environments import Pendulum, environment, spaces
 
 import jax_ppo
+
+
+class MaskedPendulum(Pendulum):
+    """
+    Subclass pendulum environment that does not return velocity
+    component of the original observation.
+    """
+
+    def get_obs(self, state):
+        return super(MaskedPendulum, self).get_obs(state)[:-1]
+
+    def observation_space(self, params):
+        high = jnp.array([1.0, 1.0], dtype=jnp.float32)
+        return spaces.Box(-high, high, shape=(2,), dtype=jnp.float32)
 
 
 def _reset_env(
